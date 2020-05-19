f3157f5a4756fa0d573bf8c6e17650fd38f7640d
Chandrasekar Perumal <chandra.sekar@affinitext.com>
2020-05-20 01:18:38 +0530

    Added MIDI support

--- Arduino-web-piano-MIDI/web/js/arduinokeyboard.js
+++ Arduino-web-piano-MIDI/web/js/arduinokeyboard.js
@@ -1,7 +1,14 @@
+// Globals to access them later.
+let midiIn = [];
+let notesOn = new Map();
+connect();
+
+
 var TotalOctaves = 3;
 var firstOctave = 3;
 var soundSelect = 0;
 
+var duration = 2;
 var record = '';
 var recordData = '';
 
@@ -140,6 +147,7 @@ function processKeys(key_values, switch_values, joy_values, knob_value, slider_v
 
 
 function playNote(note, octave, duration){
+	document.getElementById("notesdata").innerHTML += note+octave;
 	src = __audioSynth.generate(soundSelect, note, octave, duration);
 	container = new Audio(src);
 	container.addEventListener('ended', function() { container = null; });
@@ -170,4 +178,89 @@ function visualKeyGrey(note, octave){
 	document.getElementById(cur_note).style.backgroundColor = 'rgb(224, 224, 224)';
 	document.getElementById(cur_note).style.marginTop = '';
 	document.getElementById(cur_note).style.boxShadow = '';
+}
+
+
+// Start up WebMidi.
+function connect() {
+  navigator.requestMIDIAccess()
+  .then(
+    (midi) => midiReady(midi),
+    (err) => console.log('Something went wrong', err));
+}
+
+function midiReady(midi) {
+  // Also react to device changes.
+  midi.addEventListener('statechange', (event) => initDevices(event.target));
+  initDevices(midi);
+}
+
+function initDevices(midi) {
+  // Reset.
+  midiIn = [];
+  midiOut = [];
+
+  // MIDI devices that send you data.
+  const inputs = midi.inputs.values();
+  for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
+    midiIn.push(input.value);
+  }
+
+  displayDevices();
+  startListening();
+}
+
+function displayDevices() {
+  selectIn.innerHTML = midiIn.map(device => `<option>${device.name}</option>`).join('');
+
+}
+
+function startListening() {
+  outputIn.innerHTML = '';
+
+  // Start listening to MIDI messages.
+  for (const input of midiIn) {
+    input.addEventListener('midimessage', midiMessageReceived);;
+  }
+}
+
+
+function midiMessageReceived(event) {
+  // MIDI commands we care about. See
+  // http://webaudio.github.io/web-midi-api/#a-simple-monophonic-sine-wave-midi-synthesizer.
+  const NOTE_ON = 9;
+  const NOTE_OFF = 8;
+
+  const cmd = event.data[0] >> 4;
+  const pitch = event.data[1];
+  const velocity = (event.data.length > 2) ? event.data[2] : 1;
+
+  // You can use the timestamp to figure out the duration of each note.
+  const timestamp = Date.now();
+
+  octave_org = Math.floor((pitch-36)/12);
+  octave = octave_org + 3;
+  key_name = keymap[pitch-(36+octave_org*12)];
+
+
+  // Note that not all MIDI controllers send a separate NOTE_OFF command for every NOTE_ON.
+  if (cmd === NOTE_OFF || (cmd === NOTE_ON && velocity === 0)) {
+  		visualKeyRelease(key_name, octave); // -------------------------------------------
+    	// outputIn.innerHTML += `note off: pitch:<b>${pitch}</b>, velocity: <b>${velocity}</b> <br/>`;
+
+    // Complete the note!
+    const note = notesOn.get(pitch);
+    if (note) {
+      //outputIn.innerHTML += `pitch:<b>${pitch}</b>, duration:<b>${timestamp - note}</b> ms. <br>`;
+      notesOn.delete(pitch);
+    }
+  } else if (cmd === NOTE_ON) {
+  	    playNote(key_name, octave, duration);
+    	visualKeyPress(key_name, octave); // -------------------------------------------
+    	// outputIn.innerHTML += `note on: pitch:<b>${pitch}</b>, velocity: <b>${velocity}</b> <br/>`;
+
+    // One note can only be on at once.
+    notesOn.set(pitch, timestamp);
+  }
+
 }
